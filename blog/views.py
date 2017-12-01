b8d898ea6bfffd4b557692a212014aee549bfd7b
Álef Ribeiro <aleforion@outlook.com>
2017-12-01 09:58:34 -0300

    Criação de templates, post, e criação de formularios

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
+from blog.forms import PostForm
 from blog.models import Categoria, Post
 
 
@@ -13,7 +15,7 @@ def home(request):
         #Categoria.objects.create(name=categoria)
     todas_categorias = Categoria.objects.all()
 
-    categoria_python = Categoria.objects.get(name='c/c++')
+    #categoria_python = Categoria.objects.get(name=name)
     posts = Post.objects.filter(statu='Published')
 
     #post = Post()
@@ -45,3 +47,29 @@ def show_posts_by_category(request, categoria_id):
     }
 
     return render(request, 'blog/index.html', context)
+
+def auth(request):
+    error = False
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user == None:
+            error = True
+        else:
+            error = False
+            login(request, user)
+
+    context = {
+        "error": error
+    }
+    return render(request, 'blog/auth.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('/blog')
+
+def post_create(request):
+    form = PostForm()
+    return render(request, 'blog/post_create.html', {'form':form})
